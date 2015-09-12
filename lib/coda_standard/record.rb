584833eb19bb5812eedb64292f63137b09e0689d
Wouter Vancraeynest <wouter.vancraeynest@by2.be>
2015-09-12 23:26:11 +0200

    added unstructured communication

--- lib/coda_standard/record.rb
+++ lib/coda_standard/record.rb
@@ -12,7 +12,7 @@ module CodaStandard
       bic: /^22.{96}(.{11})/,
       amount: /^21.{29}(\d{16})/,
       old_balance: /^1.{41}(\d)(\d{15})/,
-      structured_communication: /^21.{60}(.{15})/,
+      structured_communication: /^21.{60}(.{53})/,
       currencies: /(^.+)(AED|AFN|ALL|AMD|ANG|AOA|ARS|AUD|AWG|AZN|BAM|BBD|BDT|BGN|BHD|BIF|BMD|BND|BOB|BOV|BRL|BSD|BTN|BWP|BYR|BZD|CAD|CDF|CHE|CHF|CHW|CLF|CLP|CNY|COP|COU|CRC|CUC|CUP|CVE|CZK|DJF|DKK|DOP|DZD|EGP|ERN|ETB|EUR|FJD|FKP|GBP|GEL|GHS|GIP|GMD|GNF|GTQ|GYD|HKD|HNL|HRK|HTG|HUF|IDR|ILS|INR|IQD|IRR|ISK|JMD|JOD|JPY|KES|KGS|KHR|KMF|KPW|KRW|KWD|KYD|KZT|LAK|LBP|LKR|LRD|LSL|LTL|LVL|LYD|MAD|MDL|MGA|MKD|MMK|MNT|MOP|MRO|MUR|MVR|MWK|MXN|MXV|MYR|MZN|NAD|NGN|NIO|NOK|NPR|NZD|OMR|PAB|PEN|PGK|PHP|PKR|PLN|PYG|QAR|RON|RSD|RUB|RWF|SAR|SBD|SCR|SDG|SEK|SGD|SHP|SLL|SOS|SRD|SSP|STD|SVC|SYP|SZL|THB|TJS|TMT|TND|TOP|TRY|TTD|TWD|TZS|UAH|UGX|USD|USN|USS|UYI|UYU|UZS|VEF|VND|VUV|WST|XAF|XAG|XAU|XBA|XBB|XBC|XBD|XCD|XDR|XFU|XOF|XPD|XPF|XPT|XSU|XTS|XUA|XXX|YER|ZAR|ZMW|ZWL)/
     }
 
@@ -169,11 +169,11 @@ module CodaStandard
       amount_sign + amount_integral + separator + amount_decimals
     end
 
-    def check_structured(number)
-      if number[0] == "1"
-        number.scan(CLEAN_FIELDS[:clean_structured]).join
+    def check_structured(message)
+      if message[0] == "1"
+        message.scan(CLEAN_FIELDS[:clean_structured]).join
       else
-        "not structured"
+        message[1..-1]
       end
     end
   end
